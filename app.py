7685ae69b3ad9f9d7d9f07c09e5cf5d91bc1de32
bogdan01m <152898065+bogdan01m@users.noreply.github.com>
2024-04-30 02:34:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ warnings.filterwarnings('ignore')
 
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 
 
 ####################################################################
